4fe1961a41a7342c03a472908e90da52d7e3b3be
iMaZiNe <imazine@me.com>
2017-11-09 11:12:30 +0900

    Improve promise samples

--- samples/promise.js
+++ samples/promise.js
@@ -1,26 +1,63 @@
 const util = require('../libs/util.js');
 
-const getArticle = util.promiseMockupFactory('getArticle', 'Hello {str}');
-const parseArticle = util.promiseMockupFactory('parseArticle', '{str} welcome');
-const refineArticle = util.promiseMockupFactory('refineArticle', '{str} to');
-const removeAbusing = util.promiseMockupFactory('removeAbusing', '{str} play.node();');
-const getComments = util.promiseMockupFactory('getComments', '{str} 2017');
-const refineComments = util.promiseMockupFactory('refineComments', '[{str}]');
-const registerComment = (refinedComment) => console.log('Comment', refinedComment, 'Registered successfully');
+const PROCESS_NUMBER = parseInt(process.argv[2], 10) || 100;
+const IS_DEBUG_MODE = process.argv.filter(option => option == '--no-debug').length ? false: true;
+const EXPECTED_TIME_OBJECT = util.expectedTimeObject;
 
-getArticle("iMaZiNe")
-    .then((article) => parseArticle(article, 1000))
-    .then(refineArticle)
-    .then((refinedArticle) => removeAbusing(refinedArticle, 2000))
-    .then(getComments)
-    .then((joinedComments) => {
-        let comments = joinedComments.split(' ');
-        comments.forEach((comment) => {
-            refineComments(comment, 1000)
-                .then(registerComment);
-            });
-        // let promises = comments.map((comment) => refineComments(comment, 1000));
-        // return Promise.all(promises);
-    })
-    // .then(refinedComments => refinedComments.forEach(registerComment))
-;
+const getArticle = util.promiseMockupFactory('getArticle', 'Hello {str}', EXPECTED_TIME_OBJECT, IS_DEBUG_MODE);
+const parseArticle = util.promiseMockupFactory('parseArticle', '{str} welcome', EXPECTED_TIME_OBJECT, IS_DEBUG_MODE);
+const refineArticle = util.promiseMockupFactory('refineArticle', '{str} to', EXPECTED_TIME_OBJECT, IS_DEBUG_MODE);
+const removeAbusing = util.promiseMockupFactory('removeAbusing', '{str} play.node();', EXPECTED_TIME_OBJECT, IS_DEBUG_MODE);
+const getComments = util.promiseMockupFactory('getComments', '{str} 2017', EXPECTED_TIME_OBJECT, IS_DEBUG_MODE);
+const refineComments = util.promiseMockupFactory('refineComments', '[{str}]', EXPECTED_TIME_OBJECT, IS_DEBUG_MODE);
+const registerComment = (refinedComment) => {
+    console.log('Comment', refinedComment, 'Registered successfully');
+    return refinedComment;
+};
+
+const getMigrationProcess = (index) => {
+    return getArticle("iMaZiNe_" + index)
+        .then((article) => parseArticle(article, 80))
+        .then(refineArticle)
+        .then(removeAbusing)
+        .then(getComments)
+        .then((joinedComments) => {
+            let comments = joinedComments.split(' ');
+        //     comments.forEach((comment) => {
+        //         refineComments(comment, 1000)
+        //             .then(registerComment);
+        //     });
+        // });
+            let commentMigrationProcesses = comments.map((comment) => refineComments(comment, 100));
+            return Promise.all(commentMigrationProcesses);
+        })
+        .then((refinedComments) => {
+            return refinedComments.map(registerComment);
+        });
+};
+
+console.info("Promise sample - %s", PROCESS_NUMBER);
+
+let startTime = util.getThisNanoSecond();
+// Single process
+// getMigrationProcess(0).then((res) => {
+//     let elapsedTime = util.getElapsedTime(startTime);
+//     util.displayAnalizedResult(EXPECTED_TIME_OBJECT.expectedTime, elapsedTime);
+// });
+
+let processArray = new Array(PROCESS_NUMBER);
+let migrationProcesses = processArray.fill(null).map((process, idx) => getMigrationProcess(idx));
+
+// Using forEach
+// migrationProcesses.forEach((process) => {
+//     process.then(res => {
+//         let elapsedTime = util.getElapsedTime(startTime);
+//         util.displayAnalizedResult(EXPECTED_TIME_OBJECT.expectedTime, elapsedTime);
+//     });
+// });
+
+// Using PromiseAll
+Promise.all(migrationProcesses).then(res => {
+        let elapsedTime = util.getElapsedTime(startTime);
+        util.displayAnalizedResult(EXPECTED_TIME_OBJECT.expectedTime, elapsedTime);
+});
